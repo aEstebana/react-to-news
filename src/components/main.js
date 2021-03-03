56af3a570b08f105ff2aa634d8823fb7c5a9226e
astevenaa <astevenaa@gmail.com>
2021-03-02 22:27:43 -0700

    added logic to main

--- src/components/main.js
+++ src/components/main.js
@@ -35,4 +35,21 @@ const Main = () => {
       console.log(customNewsSelector.customNews);
     }
   };
+
+  let news;
+  if (customNewsSelector.customNews.length > 0) {
+    news = (
+      <div className="news">
+        {customNewsSelector.customNews.map((x) => (
+          <div className="post" key={x.title}>
+            <img src={x.urlToImage} alt={x.title} />
+            <h2>{x.title}</h2>
+            <p>{x.description}</p>
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    news = <p>slect a source and relevance from the</p>;
+  }
 };
