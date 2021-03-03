90e8b998a1dc6402d1c5d5ef001d2afaf31245f3
astevenaa <astevenaa@gmail.com>
2021-03-02 22:18:10 -0700

    added to main.js

--- src/components/main.js
+++ src/components/main.js
@@ -7,10 +7,32 @@ const Main = () => {
   const [source, setSource] = useState('');
   const [relevance, setRelevance] = useState('');
 
-  const customNewSelector = useSelector((state) => state.CustomSearch);
+  const customNewsSelector = useSelector((state) => state.CustomSearch);
 
   const dispatch = useDispatch();
 
   const getCustomNews = (source, relevance) =>
     dispatch(fetchCustomNews(source, relevance));
+
+  const getSources = () => {
+    fetch('https://newapi.org/v1/sources?').then((res) => {
+      console.log(customNewsSelector.customNews);
+      return res.json();
+    });
+  };
+
+  useEffect(() => {
+    getSources();
+  });
+
+  const getNews = (e) => {
+    console.log(source);
+    e.preventDefault();
+    if (source === '' || source === 'nothing') {
+      console.log('there is no source selected');
+    } else {
+      getCustomNews(source, relevance);
+      console.log(customNewsSelector.customNews);
+    }
+  };
 };
