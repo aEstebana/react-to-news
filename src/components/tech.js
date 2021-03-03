e87837cc9bf8a1c91e14761cfafb3504bbdf0bf3
astevenaa <astevenaa@gmail.com>
2021-03-02 22:03:36 -0700

    added tech componet to search for tech related news

--- src/components/tech.js
+++ src/components/tech.js
@@ -1,3 +1,32 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import{ fetchTech}
+import { fetchTech } from '../actions/fetchTech';
+
+const Tech = () => {
+  const techSelector = useSelector((state) => state.FetchTech);
+  const dispatch = useDispatch();
+  const getTechNews = () => dispatch(fetchTech());
+
+  useEffect(() => {
+    getTechNews();
+  }, []);
+
+  return (
+    <>
+      <section>
+        <h2> Technology News</h2>
+        <div className="news">
+          {techSelector.techNews.map((x) => (
+            <div className="post" key={x.title}>
+              <img src={x.urlToImage} />
+              <h2>{x.title}</h2>
+              <p>{x.description}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Tech;
