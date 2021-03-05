b5ec886b7414471a98b36d3e24583ba0a895e04a
astevenaa <astevenaa@gmail.com>
2021-03-04 23:48:59 -0700

    added to read me

--- src/actions/fetchCustomNews.js
+++ src/actions/fetchCustomNews.js
@@ -2,7 +2,7 @@ const apiNewKey = 'cbc098e20969447c81b6cec58b3372b4';
 export function fetchCustomNews(source, relevance) {
   return function (dispatch) {
     fetch(
-      `https://newsapi.org/v1/articles?source=${source}&sortBy=${relevance}&apiKey=cbc098e20969447c81b6cec58b3372b4`
+      `https://newsapi.org/v1/articles?source=${source}&sortBy=${relevance}&apiKey=${apiNewKey}`
     )
       .then((res) => res.json())
       .then((res) => {
